0689c64c22e0d06c67fbeedb83540b978c588a5c
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-06-01 16:32:06 +0200

    Modification des squelettes pour enlever les commentaires inutiles

--- makefile_data/squelettes/Xxx.cpp
+++ makefile_data/squelettes/Xxx.cpp
@@ -8,49 +8,12 @@
 
 //---------- Réalisation de la classe <Xxx> (fichier Xxx.cpp) ------------
 
-//---------------------------------------------------------------- INCLUDE
-
-//-------------------------------------------------------- Include système
 #include <iostream>
 using namespace std;
 
-//------------------------------------------------------ Include personnel
 #include "Xxx.h"
 
-//------------------------------------------------------------- Constantes
-
-//----------------------------------------------------------------- PUBLIC
-
-//----------------------------------------------------- Méthodes publiques
-// type Xxx::Méthode ( liste des paramètres )
-// Algorithme :
-//
-//{
-//} //----- Fin de Méthode
-
-//------------------------------------------------- Surcharge d'opérateurs
-Xxx &Xxx::operator=(const Xxx &autreXxx)
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel a l'operateur d'affectation de <Xxx> sur " << this << " et " << &autreXxx << endl;
-#endif
-} //----- Fin de operator =
-
-//-------------------------------------------- Constructeurs - destructeur
-Xxx::Xxx(const Xxx &autreXxx)
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au constructeur de copie de <Xxx> sur " << this << " et " << &autreXxx << endl;
-#endif
-} //----- Fin de Xxx (constructeur de copie)
-
 Xxx::Xxx()
-// Algorithme :
-//
 {
 #ifdef MAP
     cout << "Appel au constructeur de <Xxx> sur " << this << endl;
@@ -58,14 +21,8 @@ Xxx::Xxx()
 } //----- Fin de Xxx
 
 Xxx::~Xxx()
-// Algorithme :
-//
 {
 #ifdef MAP
     cout << "Appel au destructeur de <Xxx> sur " << this << endl;
 #endif
 } //----- Fin de ~Xxx
-
-//------------------------------------------------------------------ PRIVE
-
-//----------------------------------------------------- Méthodes protégées
--- makefile_data/squelettes/Xxx.h
+++ makefile_data/squelettes/Xxx.h
@@ -10,12 +10,7 @@
 #if !defined(XXX_H)
 #define XXX_H
 
-//--------------------------------------------------- Interfaces utilisées
 using namespace std;
-
-//------------------------------------------------------------- Constantes
-
-//------------------------------------------------------------------ Types
 
 //------------------------------------------------------------------------
 // Rôle de la classe <Xxx>
@@ -25,50 +20,12 @@ using namespace std;
 
 class Xxx
 {
-    //----------------------------------------------------------------- PUBLIC
-
 public:
-    //----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    //------------------------------------------------- Surcharge d'opérateurs
-    Xxx &operator=(const Xxx &autreXxx);
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    //-------------------------------------------- Constructeurs - destructeur
-    Xxx(const Xxx &autreXxx);
-    // Mode d'emploi (constructeur de copie) :
-    //
-    // Contrat :
-    //
-
     Xxx();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
 
     virtual ~Xxx();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    //------------------------------------------------------------------ PRIVE
 
 protected:
-    //----------------------------------------------------- Méthodes protégées
-
-    //----------------------------------------------------- Attributs protégés
 };
-
-//-------------------------------- Autres définitions dépendantes de <Xxx>
 
 #endif // XXX_H
