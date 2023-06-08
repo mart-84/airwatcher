619cbd46e51294417e88acabf32c8a87f12d32b6
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-06-08 16:06:35 +0200

    Feu d'artifice

--- tests/test.h
+++ tests/test.h
@@ -7,22 +7,21 @@ std::vector<std::pair<std::string, void (*)()>> tests = {};
 
 #define REGISTER_TEST(test) tests.push_back(make_pair(#test, test));
 
-#define RUN_TESTS()                                                             \
-    {                                                                           \
-        for (auto &test : tests)                                                \
-        {                                                                       \
-            test.second();                                                      \
-            std::cout << test.first << " \033[1;32mpassed\033[0m" << std::endl; \
-        }                                                                       \
-        std::cout << tests.size() << " tests passed" << std::endl;              \
+#define RUN_TESTS()                                                                    \
+    {                                                                                  \
+        for (auto &test : tests)                                                       \
+        {                                                                              \
+            test.second();                                                             \
+            std::cout << test.first << " \033[1;32mpassed\033[0m" << std::endl;        \
+        }                                                                              \
+        std::cout << tests.size() << " tests passed" << std::endl;                     \
+        std::cout << "                                   .''. " << endl;               \
+        std::cout << "       .''.      .        *''*    :_\\/_:     . " << endl;       \
+        std::cout << "      :_\\/_:   _\\(/_  .:.*_\\/_*   : /\\ :  .'.:.'. " << endl; \
+        std::cout << "  .''.: /\\ :   ./)\\   ':'* /\\ * :  '..'.  -=:o:=- " << endl;  \
+        std::cout << " :_\\/_:'.:::.    ' *''*    * '.\'/.' _\\(/_'.':'.' " << endl;   \
+        std::cout << " : /\\ : :::::     *_\\/_*     -= o =-  /)\\    '  * " << endl;  \
+        std::cout << "  '..'  ':::'     * /\\ *     .'/.\'.   ' " << endl;             \
+        std::cout << "      *            *..*         : " << endl;                     \
+        std::cout << "        * " << endl;                                             \
     }
-
-//                                    .''.
-//        .''.      .        *''*    :_\/_:     .
-//       :_\/_:   _\(/_  .:.*_\/_*   : /\ :  .'.:.'.
-//   .''.: /\ :   ./)\   ':'* /\ * :  '..'.  -=:o:=-
-//  :_\/_:'.:::.    ' *''*    * '.\'/.' _\(/_'.':'.'
-//  : /\ : :::::     *_\/_*     -= o =-  /)\    '  *
-//   '..'  ':::'     * /\ *     .'/.\'.   '
-//       *            *..*         :
-//         *
